a0b78ffaa397730902f271de8c06223aff07c165
Zachcodes <Zaspringer@gmail.com>
2018-01-21 20:18:55 -0700

    added process environment variables and some packages

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const path = require('path');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const connectionString = process.env.connectionString;
 
 // Priority serve any static files.
 app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
